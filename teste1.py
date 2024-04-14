97b7d818f960b1a9270da2887973734ef02d1e75
victorhugochrisosthemos <victor_lampert@outlook.com>
2024-04-14 20:34:12 -0300

    first commit

---
+++ teste1.py
@@ -0,0 +1,14 @@
+# pip install scipy
+# pip install numpy
+# pip install scikit-learn
+# pip install ipython
+# pip install ipython[notebook]
+# jupyter notebook
+# para fechar o servidor criado: ctrl + c  e yes
+# pip install matplotlib
+
+#Para o advertising
+#pip install pandas
+#pip install seaborn
+#https://www.statlearning.com/resources-first-edition
+#
